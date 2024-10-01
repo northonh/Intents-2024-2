439b750876cd72ade5dacff3ef927908dd7aba9c
northonh <nobile.pedro@gmail.com>
2024-09-30 22:30:51 -0300

    Demais Intents tratadas em sala.

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
@@ -1,7 +1,21 @@
 package br.edu.ifsp.scl.ads.pdm.intents
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.content.Intent.ACTION_CHOOSER
+import android.content.Intent.ACTION_DIAL
+import android.content.Intent.ACTION_PICK
+import android.content.Intent.ACTION_VIEW
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.EXTRA_TITLE
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
@@ -22,6 +36,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var parl: ActivityResultLauncher<Intent>
+    private lateinit var pcarl: ActivityResultLauncher<String>
+    private lateinit var piarl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,12 +57,91 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        pcarl = registerForActivityResult(ActivityResultContracts.RequestPermission())
+        { permissaoConcedida ->
+            if (permissaoConcedida) {
+                chamarOuDiscar(true)
+            }
+            else {
+                Toast.makeText(this, "Permissão necessária!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        piarl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { resultado ->
+            if (resultado.resultCode == RESULT_OK) {
+                resultado.data?.data?.let {
+                    startActivity(Intent(ACTION_VIEW, it))
+                }
+            }
+        }
+
         amb.entrarParametroBt.setOnClickListener {
             Intent("MINHA_ACTION_PARA_PROXIMA_TELA").apply {
                 amb.parametroTv.text.toString().let{
                     putExtra(PARAMETRO_EXTRA, it)
                 }
                 parl.launch(this)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.viewMi -> {
+                Uri.parse(amb.parametroTv.text.toString()).let {
+                    Intent(ACTION_VIEW, it).apply {
+                        startActivity(this)
+                    }
+                }
+                true
+            }
+            R.id.callMi -> {
+                if (checkSelfPermission(CALL_PHONE) == PERMISSION_GRANTED) {
+                    chamarOuDiscar(true)
+                }
+                else {
+                    pcarl.launch(CALL_PHONE)
+                }
+                true
+            }
+            R.id.dialMi -> {
+                chamarOuDiscar(false)
+                true
+            }
+            R.id.pickMi -> {
+                val caminho = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES
+                ).path
+                val pegarImagemIntent = Intent(ACTION_PICK)
+                pegarImagemIntent.setDataAndType(Uri.parse(caminho), "image/*")
+                piarl.launch(pegarImagemIntent)
+                true
+            }
+            R.id.chooserMi -> {
+                Uri.parse(amb.parametroTv.text.toString()).let { url ->
+                    Intent(ACTION_VIEW, url).let { navegadorIntent ->
+                        val escolherAppIntent = Intent(ACTION_CHOOSER)
+                        escolherAppIntent.putExtra(EXTRA_TITLE, "Escolha seu navegador")
+                        escolherAppIntent.putExtra(EXTRA_INTENT, navegadorIntent)
+                        startActivity(escolherAppIntent)
+                    }
+                }
+                true
+            }
+            else -> { false }
+        }
+    }
+
+    private fun chamarOuDiscar(chamar: Boolean) {
+        Uri.parse("tel: ${amb.parametroTv.text}").let {
+            Intent(if (chamar) ACTION_CALL else ACTION_DIAL).apply {
+                data = it
+                startActivity(this)
             }
         }
     }
